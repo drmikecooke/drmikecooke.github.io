fc2a452d585f11bf8947914790232a3f07fcd943
Mike Cooke <drmikecooke@gmail.com>
2020-11-29 11:57:02 +0000

    Smaller mike image

--- lcard.js
+++ lcard.js
@@ -1,5 +1,4 @@
 function lcard(href, head, img) {
-  var html = '<a href = "' + href + '"><div class="cardh-250px"><h2>' + head +
+document.getElementById("lcards").innerHTML += '<a href = "' + href + '"><div class="cardh-250px"><h2>' + head +
       '</h2><img class="h-180px" src="' + img + '" alt="' + img + '" /></div></a>';
-  document.write(html);
 }
