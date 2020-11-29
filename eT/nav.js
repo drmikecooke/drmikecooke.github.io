13ee879d6d63030dc14e121a5e22bab544bce6d8
Mike Cooke <drmikecooke@gmail.com>
2020-11-29 15:24:27 +0000

    Some more changes

--- eT/nav.js
+++ eT/nav.js
@@ -1,8 +1,9 @@
-document.write('<a href="/" style="float:right">Back to base</a>');
-document.write('<a href="/eT/eTP.html">Prospectus</a>');
-document.write('<a href="/eT/fi/fi.html">Infection delays</a>');
-document.write('<a href="/eT/SIR/SIR.html">SIR model and covid-19</a>');
-document.write('<a href="/eT/Power/Power.html">Powering<sup>up</sup></a>');
-document.write('<a href="/eT/Branching/Branching.html">Bugs, bunnies . . .</a>');
-document.write('<a href="/eT/Logjam/logjam.html">Logjam</a>');
-document.write('<a href="/eT/Backlog/backlog.html">Backlog</a>');
+var navbar = document.getElementById("menu");
+navbar.innerHTML += '<a href="/" style="float:right">Back to base</a>\n';
+navbar.innerHTML += '<a href="/eT/eTP.html">Prospectus</a>\n';
+navbar.innerHTML += '<a href="/eT/fi/fi.html">Infection delays</a>\n';
+navbar.innerHTML += '<a href="/eT/SIR/SIR.html">SIR model and covid-19</a>\n';
+navbar.innerHTML += '<a href="/eT/Power/Power.html">Powering<sup>up</sup></a>\n';
+navbar.innerHTML += '<a href="/eT/Branching/Branching.html">Bugs, bunnies . . .</a>\n';
+navbar.innerHTML += '<a href="/eT/Logjam/logjam.html">Logjam</a>\n';
+navbar.innerHTML += '<a href="/eT/Backlog/backlog.html">Backlog</a>\n';
