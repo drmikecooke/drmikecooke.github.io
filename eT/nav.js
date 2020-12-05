132135a692260c58c9742f5e03a7f0923a1c1a7e
Mike Cooke <drmikecooke@gmail.com>
2020-12-05 16:33:45 +0000

    Dropdown eT menus

--- eT/nav.js
+++ eT/nav.js
@@ -4,8 +4,7 @@ navbar.innerHTML += '<a href="/eT/eTP.html">Prospectus</a>\n';
 navbar.innerHTML += '<a href="/eT/fi/fi.html">Infection delays</a>\n';
 navbar.innerHTML += '<a href="/eT/SIR/SIR.html">SIR model and covid-19</a>\n';
 navbar.innerHTML += '<a href="/eT/Power/Power.html">Powering<sup>up</sup></a>\n';
-navbar.innerHTML += '<a href="/eT/Branching/Branching.html">Bugs, bunnies . . .</a>\n';
-navbar.innerHTML += '<a href="/eT/Logjam/logjam.html">Logjam</a>\n';
 navbar.innerHTML += '<a href="/eT/Backlog/backlog.html">Backlog</a>\n';
-navbar.innerHTML += '<a href="/eT/Basic analysis/basic.html">Basic analysis</a>\n';
-navbar.innerHTML += '<a href="/eT/Richardson/Richardson.html">Richardson extrapolation</a>\n';
+navbar.innerHTML += '<div class="dropdown"><button class="dropbtn">Logjam ▼</button><div class="dropdown-content"><a href="/eT/Logjam/logjam0.html">0 Introduction</a><a href="/eT/Logjam/logjam1.html">1 So what are logarithms?</a><a href="/eT/Logjam/logjam2.html">2 Power relations</a><a href="/eT/Logjam/logjam3.html">3 Tabling some vulgarity</a><a href="/eT/Logjam/logjam4.html">4 Compound uselessness</a><a href="/eT/Logjam/logjam5.html">5 I hear you</a><a href="/eT/Logjam/logjam6.html">6 Graph-o-log-y or log-graph-y?</a><a href="/eT/Logjam/logjam7.html">7 What the frack is a slide-rule?</a></div></div>\n'
+navbar.innerHTML += '<div class="dropdown"><button class="dropbtn">Analysis ▼</button><div class="dropdown-content"><a href="/eT/Basic analysis/basic.html">Basic</a><a href="/eT/Richardson/Richardson.html">Richardson extrapolation</a></div></div>\n'
+navbar.innerHTML += '<div class="dropdown"><button class="dropbtn">Bugs, bunnies . . . ▼</button><div class="dropdown-content"><a href="/eT/Branching/Branching1.html">1 Branches</a><a href="/eT/Branching/Branching2.html">2 Wascally wabbits</a><a href="/eT/Branching/Branching3.html">3 Boom!</a></div></div>\n'
