89ac52381190e8cbb92cbca796a5a158312f0fbc
Mike Cooke <drmikecooke@gmail.com>
2020-12-07 14:12:59 +0000

    Automate ST

---
+++ ST/nav.js
@@ -0,0 +1,5 @@
+var navbar = document.getElementById("menu");
+navbar.innerHTML = '<a href="/index.html">Base</a>';
+navbar.innerHTML += '<a href="2020.html">2020</a>';
+navbar.innerHTML += '<a href="2019.html">2019</a>';
+navbar.innerHTML += '<a href="2018.html">2018</a>';
