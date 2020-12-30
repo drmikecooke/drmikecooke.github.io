537a6378ac844a399ad0c7395245fc50df136e78
Mike Cooke <drmikecooke@gmail.com>
2020-12-30 16:41:39 +0000

    Tidy up

--- Other/Factor/Factor.js
+++ Other/Factor/Factor.js
@@ -2,76 +2,67 @@ function gt0(a) {
     return a>0 // greater than zero function
 }
 
-function tcol(nlist,plist,ilist) {
+function tcol(plist) {
     var tstring = "<table>\n"; // set up table
     tstring += "<tr><th>Number</th><th>Factors</th></tr>\n"; // top row with descriptions
-    for (var i = 0; i < nlist.length; i++) {
-        // output table rows with number and formatted prime factorization
-        tstring += "<tr><td>" + nlist[i] + "</td><td>";
-        var ps = plist[i];
-        var is = ilist[i];
+    for (var i = 0; i < plist.length; i++) {
+        // output table row with number and formatted prime factorization
+        tstring += "<tr><td>" + plist[i][0] + "</td><td>";
+        
+        var ps = plist[i][1];
+        var is = plist[i][2];
             
         for (var j = 0; j < ps.length; j++) {
             tstring += ps[j] + "<sup>" + is[j] + "</sup>"; // prime + superscript index
         }
             
         tstring += "</td></tr>\n"; // end row
-    }
-        
+    }        
     return tstring + "</table>\n";
 }
 
-function dsieve(ls) {
+function dsieve(wl, pl) {
     // sieve lists to extract prime factors
-    var nl, pl, il, nxt, nm, np, st;
-    [nl, pl, il] = ls; // number, prime, index lists
-    nxt = nl.findIndex(gt0); // find first non-zero element index in nl
-    nm = nl.length; // list length
-    np = nl[nxt]; // prime for dividing selected numbers
+    var nl, nxt, nm, np, st;
+    nxt = wl.findIndex(gt0); // find first non-zero element index in work array
+    nm = wl.length; // list length
+    np = wl[nxt]; // prime for dividing selected numbers
     st = nxt + 2; // step between divisions
     
     for (var i = nxt; i < nm; i = i + st){
-        nl[i] = nl[i]/np; // divide selected number by prime
-        if (pl[i].includes(np)) {
-            var j = pl[i].indexOf(np); // find position of prime in list
-            il[i][j]++;
+        wl[i] = wl[i]/np; // divide selected number by prime
+        var j = pl[i][1].indexOf(np); // find position of prime in list
+        if (j!=-1) {
+            pl[i][2][j]++;
         } else {
-            //add prime to prime list as not already present
-            pl[i] = pl[i].concat([np]);
-            il[i] = il[i].concat([1]);
+            pl[i][1].push(np); //add prime to prime list if not already present
+            pl[i][2].push(1); // index of 1
         }
-        nl[nxt] = 0; // sieve step complete so zero work list entry
     }
-    return [nl, pl, il]
+    wl[nxt] = 0; // sieve step complete so zero work list entry
 }
 
 function PrimeList(nMax) {
-    var nl = [], ol = [], pl = [], il = [], wlist, llist;
+    var wl = [], pl = [], wlist, llist;
     for (var i = 2; i < nMax + 1; i++) {
-        nl.push(i); // working number array
-        pl.push([]); // prime content
+        wl.push(i); // working number array
+        pl.push([i,[],[]]); // prime list framework: number, prime content, indices
     }
-    ol = [...nl]; // clone number array
-    il = [...pl]; // clone content array for indices
     
-    llist = [nl,pl,il]; // component list for sieve
-        
     for (var i =0; i < nMax -1; i++) {
-        if (llist[0][i]>1) {
-            llist = dsieve(llist); // perform sieve step
+        if (wl[i]>1) {
+            dsieve(wl, pl); // perform sieve step
         } else {
-            llist[0][i] = 0; // zero leading 1s from work list
+            wl[i] = 0; // zero leading 1s from work list
         }
     }
         
-    this.nlist = ol;
-    this.plist = llist[1];
-    this.ilist = llist[2];
+    this.plist = pl;
     this.table = function (numcol) {
-        var dslice = Math.ceil(this.nlist.length/numcol), tspread='<div style="display:inline-block;margin:auto;">', i;
-        for (i= 0; i < this.nlist.length; i=i+dslice) {
-            tspread += "<div style='float:left;padding:6px;'>";
-            tspread += tcol(this.nlist.slice(i,i+dslice),this.plist.slice(i,i+dslice),this.ilist.slice(i,i+dslice));
+        var dslice = Math.ceil(this.plist.length/numcol), tspread='<div class="centerblock">', i;
+        for (i= 0; i < this.plist.length; i=i+dslice) {
+            tspread += "<div class='tablecolumn'>";
+            tspread += tcol(this.plist.slice(i,i+dslice));
             tspread += "</div>"
         }
         return tspread+"</div>"; 
