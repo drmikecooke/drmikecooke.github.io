3079b712d9937d02b81dfa6eea5825f6d46630a2
Mike Cooke <drmikecooke@gmail.com>
2020-12-30 16:26:34 +0000

    footer

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,4 @@
-document.getElementById("email").innerHTML='<a href="mailto:mike@mikecookefreelance.com">mike@mikecookefreelance.com</a>\n';
+var footer = document.createElement('footer');
+footer.innerHTML = '<a href="mailto:mike@mikecookefreelance.com" style="text-decoration:none;color:black;">mike@mikecookefreelance.com</a>\n';
+footer.style = 'padding:5px;text-align:center;background:#ccc;font-weight:bold;font-size:21px;color:black;font-family: Arial, Helvetica, sans-serif;';
+document.body.appendChild(footer);
