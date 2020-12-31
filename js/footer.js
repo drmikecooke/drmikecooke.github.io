7dfe2807bbbe1d53b7b4bc79662f051e19d57112
Mike Cooke <drmikecooke@gmail.com>
2020-12-31 17:13:13 +0000

    Nav to js and main index

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,11 @@
 var footer = document.createElement('footer');
-footer.innerHTML = '<a href="mailto:mike@mikecookefreelance.com" style="text-decoration:none;color:black;">mike@mikecookefreelance.com</a>\n';
-footer.style = 'padding:5px;text-align:center;background:#ccc;font-weight:bold;font-size:21px;color:black;font-family: Arial, Helvetica, sans-serif;';
+var a = document.createElement('a');
+var style = "";
+a.href = "mailto:mike@mikecookefreelance.com";
+a.text = "mike@mikecookefreelance.com";
+if(window.innerWidth<900) {style="font-size:18px;"} else {style="font-size:21px;"}
+style += "font-weight:bold;color:black;font-family: Arial, Helvetica, sans-serif;text-decoration:none;color:black;"
+a.style = style;
+footer.style = 'padding:5px;background:#ccc;text-align:center;';
 document.body.appendChild(footer);
+footer.appendChild(a);
--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,16 @@
-var navbar = document.getElementById("menu");
+function anchor(url,text) {return '<a href="'+url+'">'+text+'</a>'}
+
+var navstyle = document.createElement("style");
+navstyle.innerText = "nav{font-family:Franklin;font-size:18px;overflow:hidden;background-color:#333;user-select:none;}"
+navstyle.innerText += "nav a{display:inline-block;color:#f2f2f2;text-align:center;padding:14px 16px;text-decoration:none;}";
+navstyle.innerText += "nav a:hover{background-color:#ddd;color:black;}";
+document.body.appendChild(navstyle);
+navbar = document.createElement("nav");
 navbar.innerHTML = '<a href="/" style="float:right">Back to base</a>\n';
-navbar.innerHTML +='<a href="/">Base</a>\n';
-navbar.innerHTML +='<a href="/eT/fi/fi0.html">Infection delays</a>\n';
-navbar.innerHTML +='<a href="eT/"><span class="exponential">e<em><sup>x</sup>ponential</em></span> Times</a>\n';
-navbar.innerHTML +='<a href="/eT/SIR/SIR0.html">SIR model and covid-19</a>\n';
-navbar.innerHTML +='<a href="/ST/2020.html">Semiconductor Today</a>\n';
-navbar.innerHTML +='<a href="/Other">Website experiments</a>\n';
+navbar.innerHTML += anchor("/","Base");
+navbar.innerHTML += anchor("/eT/fi/fi0.html","Infection delays");
+navbar.innerHTML += anchor("eT/",'<span class="exponential">e<em><sup>x</sup>ponential</em></span> Times');
+navbar.innerHTML += anchor("/eT/SIR/SIR0.html","SIR model and covid-19");
+navbar.innerHTML += anchor("/ST/2020.html","Semiconductor Today");
+navbar.innerHTML += anchor("/Other","Website experiments");
+document.body.appendChild(navbar);
