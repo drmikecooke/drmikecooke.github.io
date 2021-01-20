5c2d576ed2eee83f03da798f1a73ca8121f7596a
Mike Cooke <drmikecooke@gmail.com>
2021-01-20 15:30:03 +0000

    Infection delay accumulation and corrections

--- js/lpanel.js
+++ js/lpanel.js
@@ -7,7 +7,7 @@ container.appendChild(lpanel);
 lpanel.innerHTML += lcard("/eT/SIR/SIR5.html", "SIR, December 2020 update", "/images/SIR2012.svg");
 lpanel.innerHTML += lcard("/eT/fi/fi4.html", "Drowning and waving", "/eT/fi/images/weekly.svg");
 lpanel.innerHTML += lcard("/eT/fi/fi0.html", "Infection delays", "/eT/fi/images/fi.svg");
-lpanel.innerHTML += lcard("/eT/SIR/SIR.html", "SIR model and covid-19", "/eT/SIR/images/SIR.svg");
+lpanel.innerHTML += lcard("/eT/SIR/SIR0.html", "SIR model and covid-19", "/eT/SIR/images/SIR.svg");
 lpanel.innerHTML += lcard("/eT", '<span class="exponential">e<em><sup>x</sup>ponential</em></span> <span class="frank">Times</span>', "/images/burn.webp");
 lpanel.innerHTML += lcard("/eT/Branching/Branching1.html", "Bugs, bunnies, . . .", "/eT/images/branch.svg");
 lpanel.innerHTML += lcard("/eT/Logjam/logjam0.html", "Logjam", "/images/tags.webp");;
