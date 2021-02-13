09a1546a915e300e442527aab39e20eadd7ae07f
Mike Cooke <drmikecooke@gmail.com>
2021-02-13 20:28:04 +0000

    ST Feb 2

--- js/lpanel.js
+++ js/lpanel.js
@@ -6,7 +6,7 @@ lpanel.className = "panel";
 container.appendChild(lpanel);
 lpanel.innerHTML += lcard("/eT/SIR/SIR5.html", "SIR, December 2020 update", "/images/SIR2012.svg");
 lpanel.innerHTML += lcard("/eT/fi/fi4.html", "Drowning and waving", "/eT/fi/images/weekly.svg");
-lpanel.innerHTML += lcard("/eT/fi/fi0.html", "Infection delays", "/eT/fi/images/fi.svg");
+lpanel.innerHTML += lcard("/eT/fi/fi0.html", "Infection delays", "/images/infdel.png");
 lpanel.innerHTML += lcard("/eT/SIR/SIR0.html", "SIR model and covid-19", "/eT/SIR/images/SIR.svg");
 lpanel.innerHTML += lcard("/eT", '<span class="exponential">e<em><sup>x</sup>ponential</em></span> <span class="frank">Times</span>', "/images/burn.webp");
 lpanel.innerHTML += lcard("/eT/Branching/Branching1.html", "Bugs, bunnies, . . .", "/eT/images/branch.svg");
