34107282cac6042c913b92277d5a0a9204f7333a
Mike Cooke <drmikecooke@gmail.com>
2021-06-12 17:04:58 +0100

    Convergence and rooted start

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,11 @@
 var footer = document.createElement('footer');
-var a = document.createElement('a');
+var anchor = document.createElement('a');
 var style = "";
-a.href = "mailto:mike@mikecookefreelance.com";
-a.text = "mike@mikecookefreelance.com";
+anchor.href = "mailto:mike@mikecookefreelance.com";
+anchor.text = "mike@mikecookefreelance.com";
 if(window.innerWidth<900) {style="font-size:18px;"} else {style="font-size:21px;"}
 style += "font-weight:bold;color:black;font-family: Arial, Helvetica, sans-serif;text-decoration:none;color:black;"
-a.style = style;
+anchor.style = style;
 footer.style = 'padding:5px;background:#ccc;text-align:center;';
 document.body.appendChild(footer);
-footer.appendChild(a);
+footer.appendChild(anchor);
