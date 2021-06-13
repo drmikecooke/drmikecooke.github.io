093a4e04f7e0ba6bc54668d70ced91a3b10d5efa
Mike Cooke <drmikecooke@gmail.com>
2021-06-13 14:03:59 +0100

    Converge menu js

---
+++ converge/nav.js
@@ -0,0 +1,54 @@
+function anchor(url,text){return `<a href="${url}">${text}</a>`}
+
+function bar(y){return `<rect y="${y}" width="70" height="10" rx="8" fill="white"></rect>`}
+
+function details(title,list){
+  var element=document.createElement("details");
+  var summary=document.createElement("summary");
+  summary.innerText=title;
+  element.appendChild(summary);
+  for (var item of list)element.innerHTML+=anchor(...item)
+  return element
+}
+
+var navstyle = document.createElement("style");
+navstyle.innerText = "nav{position: relative;}";
+navstyle.innerText += "input{width:40px;height:40px;position:absolute;left:5px;top:5px;cursor:pointer;opacity:0;z-index:2;}";
+navstyle.innerText += ".bars{background-color:#1E1E23;padding:10px;cursor:pointer;}";
+navstyle.innerText += ".bars svg{width:auto;}";
+navstyle.innerText += "nav a:hover{background-color:#333;color:white;}";
+navstyle.innerText += "nav a{display:block;padding:5px;}";
+navstyle.innerText += "#menupanel{font-family:Franklin;font-size:18px;position:absolute;box-shadow:0 0 10px #85888C;padding:10px;background-color:#F5F6FA;transform-origin:0% 0%;z-index:1;display:none;transition:transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);top:5px;left:5px;}";
+navstyle.innerText += "input:checked~#menupanel{display:block}";
+document.body.appendChild(navstyle);
+
+var navpanel=document.createElement("nav");
+navpanel.className="bars";
+var bars=document.createElementNS("http://www.w3.org/2000/svg", "svg");
+bars.setAttribute("viewBox","0 0 70 60");
+bars.setAttribute("width","30px");
+bars.innerHTML+=bar(0)+bar(25)+bar(50);
+navpanel.appendChild(bars);
+
+navpanel.innerHTML+='<input type="checkbox" />';
+
+var menu=document.createElement("div");
+menu.id="menupanel";
+
+var cross=document.createElementNS("http://www.w3.org/2000/svg", "svg");
+cross.setAttribute("viewBox","0 0 80 80");
+cross.setAttribute("width","30px");
+cross.innerHTML='<line x1="10" y1="10" x2="40" y2="40" stroke="red" stroke-linecap="round" stroke-width="10" />';
+cross.innerHTML+='<line x1="10" y1="40" x2="40" y2="10" stroke="red" stroke-linecap="round" stroke-width="10" />';
+menu.appendChild(cross);
+
+menu.innerHTML+=anchor('/','Base');
+menu.innerHTML+=anchor('/converge','C<span class="shrink">o<span class="shrink">n<span class="shrink">v<span class="shrink">e<span class="shrink">r<span class="shrink">g<span class="shrink">e<span class="shrink">n<span class="shrink">c<span class="shrink">e</span></span></span></span></span></span></span></span></span></span>');
+var rooted=[];
+rooted.push(['/converge/rooted/0.html','0 - A journey to infinity . . .']);
+rooted.push(['/converge/rooted/1.html','1 - Binary search']);
+rooted.push(['/converge/rooted/2.html','2 - Decimal search']);
+menu.appendChild(details("Rooted",rooted))
+navpanel.appendChild(menu)
+
+document.body.appendChild(navpanel);
