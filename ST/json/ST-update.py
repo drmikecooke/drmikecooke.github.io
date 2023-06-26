901e1d751e99d975b1c1291476d46f6bbd95c5c1
mike <drmikecooke@gmail.com>
2023-06-26 10:15:37 +0100

    ST0update.py added

---
+++ ST/json/ST-update.py
@@ -0,0 +1,33 @@
+import json
+
+def get(file):
+    with open(file, 'r') as fp:
+        return json.load(fp)
+
+def put(d,file):
+    with open(file, 'w') as fp:
+        json.dump(d, fp)
+
+def STitem(caption,img,url,ref):
+    return {'caption':caption,'img':img,'url':url,'ref':ref}
+
+def addSTitem(d,month,item):
+    if month in d.keys():
+        d[month] += [item]
+    else:
+        d = {month:[item],**d}
+    return d
+
+def inputST(d):
+    month=input('Month: ')
+    caption=input('Caption: ')
+    img=input('Image: ')
+    href=input('Link: ')
+    ref=input('Reference: ')
+    return addSTitem(d,month,STitem(caption,img,href,ref))
+
+if __name__ == "__main__":
+	j=get('2023.json')
+	put(j,'2023.json.bak')
+	j=inputST(j)
+	put(j,'2023.json')
